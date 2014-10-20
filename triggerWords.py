cf9cf931b70424ff1657e64b16a43e2e42b7f8eb
Brian Cassidy <brian.cassidy@gmail.com>
2014-10-20 14:02:21 -0300

    add new trigger

--- triggerWords.py
+++ triggerWords.py
@@ -71,5 +71,24 @@ triggers = {
                     'uri': 'http://i.imgur.com/a3Xp4Gq.jpg'
                 }
             ]
+    },
+    'welcome': {
+        'probability': 33,
+        'text': 'Welcome to The Internet! ',
+        'targets':
+            [
+                {
+                    'weight': 20,
+                    'uri': 'http://imgur.com/dOd5yIW'
+                },
+                {
+                    'weight': 20,
+                    'uri': 'http://new4.fjcdn.com/comments/3180221+_bfbd0ea585ec60d40888f3899a5ab625.jpg'
+                },
+                {
+                    'weight': 20,
+                    'uri': 'https://c2.staticflickr.com/8/7260/8169906287_ae19c2e85f_z.jpg'
+                }
+            ]
     }
 }
