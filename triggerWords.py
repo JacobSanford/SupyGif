34f3088231cf3ebc9ba7611cc2a807cb80465cc7
Jacob Sanford <jsanford@unb.ca>
2014-10-20 13:26:33 -0300

    Add new images

--- triggerWords.py
+++ triggerWords.py
@@ -2,6 +2,17 @@
 """
 
 triggers = {
+    'amaz(e|ing)': {
+        'probability': 20,
+        'text': 'Uhhh.. ',
+        'targets':
+            [
+                {
+                    'weight': 20,
+                    'uri': 'http://i.imgur.com/ivjVA.jpg'
+                }
+            ]
+    },
     'celebrat(e|ion)': {
         'probability': 25,
         'text': 'Celebrate! ',
@@ -25,6 +36,39 @@ triggers = {
                 {
                     'weight': 20,
                     'uri': 'http://i.imgur.com/YaEedqD.gif'
+                }
+            ]
+    },
+    'roommate': {
+        'probability': 70,
+        'text': 'Leah\'s Roomate ',
+        'targets':
+            [
+                {
+                    'weight': 20,
+                    'uri': 'http://i.imgur.com/RiZPQ.gif'
+                }
+            ]
+    },
+    'twer(k|king)': {
+        'probability': 70,
+        'text': 'Twerk  ',
+        'targets':
+            [
+                {
+                    'weight': 20,
+                    'uri': 'http://i.imgur.com/AZ3LdAh.gif'
+                }
+            ]
+    },
+    'worf': {
+        'probability': 80,
+        'text': 'Soon ',
+        'targets':
+            [
+                {
+                    'weight': 20,
+                    'uri': 'http://i.imgur.com/a3Xp4Gq.jpg'
                 }
             ]
     }
